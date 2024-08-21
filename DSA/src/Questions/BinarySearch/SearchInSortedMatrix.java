7df01abbc56973e39a9d53adfb3bd781d0f0a998
Adhirkam Maitra <adhikram.m@media.net>
2024-08-21 13:49:43 +0530

    📝 OP Solution

--- DSA/src/Questions/BinarySearch/SearchInSortedMatrix.java
+++ DSA/src/Questions/BinarySearch/SearchInSortedMatrix.java
@@ -28,8 +28,28 @@ public class SearchInSortedMatrix {
         return false;
     }
 
+    public static String solve(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col > -1) {
+            int cur = matrix[row][col];
+            if (cur == target) {
+                return "" + row + " " + col;
+            }
+            if (cur < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+
+        return "Element not found";
+
+    }
+
     public static void main(String[] args) {
         int[][] matrix = new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
         System.out.println(searchMatrix(matrix, 3));
+        System.out.println(solve(matrix, 3));
     }
 }
